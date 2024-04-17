b83add6b30ec5c0b48e111298531ea26481be53a
leowebdesigner <contato@leowebdesigner.com.br>
2024-04-17 00:10:01 -0300

    🔐 feature: autenticação de rotas na api com sanctum

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\Api\HotelsController;
 use App\Http\Controllers\Api\RoomsController;
 use Illuminate\Support\Facades\Route;
 
+Route::middleware(['auth:sanctum'])->group(function(){
+
 Route::get('/hotels', [HotelsController::class, 'index']);
 Route::get('/hotels/{id}', [HotelsController::class, 'show']);
 Route::post('/hotels', [HotelsController::class, 'create']);
@@ -15,6 +17,8 @@ Route::get('/rooms/{id}', [RoomsController::class, 'show']);
 Route::post('/rooms', [RoomsController::class, 'create']);
 Route::delete('/rooms/{id}', [RoomsController::class, 'delete']);
 Route::put('/rooms/{id}', [RoomsController::class, 'update']);
+
+});
 
 Route::get('/', function (){
     return response()->json([
