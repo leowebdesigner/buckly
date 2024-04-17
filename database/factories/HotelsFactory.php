923284c314687006be3c7817044ca95853a5fe01
leowebdesigner <contato@leowebdesigner.com.br>
2024-04-17 03:24:38 -0300

    🔧fix: factory agora preenche automaticamente o endereco usando service via facil

--- database/factories/HotelsFactory.php
+++ database/factories/HotelsFactory.php
@@ -3,29 +3,36 @@
 namespace Database\Factories;
 
 use App\Models\Hotels;
+use App\Services\ViaCepService;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Hotels>
- */
 class HotelsFactory extends Factory
 {
-
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
     protected $model = Hotels::class;
-    
+
     /**
      * Define the model's default state.
      *
-     * @return array<string, mixed>
+     * @return array
      */
-    public function definition(): array
+    public function definition()
     {
+        $viaCepService = app(ViaCepService::class);
+        $ceps = ['05407002', '01153000', '13484015'];
+        $cep = $this->faker->randomElement($ceps);
+        $addressData = $viaCepService->getAddressDetails($cep);
+
         return [
-            'name' => $this->faker->name(),
-            'address' => $this->faker->unique()->sentence(15),
-            'city' => $this->faker->name(),
-            'state' => $this->faker->name(),
-            'zip_code' => $this->faker->randomNumber(9, true),
+            'name' => $this->faker->company(),
+            'address' => $addressData['address'] ?? $this->faker->streetAddress(),
+            'city' => $addressData['city'] ?? $this->faker->city(),
+            'state' => $addressData['state'] ?? $this->faker->state(),
+            'zip_code' => $addressData['zip_code'] ?? $cep,
             'website' => $this->faker->url(),
         ];
     }
